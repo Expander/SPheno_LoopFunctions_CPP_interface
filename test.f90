888c30d525468a5b84b338cc2885273bcd4b78cc
Alexander Voigt <Alexander.Voigt@desy.de>
2015-12-21 12:24:21 +0100

    printing overall test result

--- test.f90
+++ test.f90
@@ -4,14 +4,23 @@
     Use LoopFunctionsSubroutines
 
     double precision :: re, im
-    Complex*16 :: res
+    Complex(dp) :: res
+    Logical :: eqre, eqim, error
 
     ! vertexC0tilde vertexC0tildeSub
-    write (*,*) "testing vertexC0tilde == vertexC0tildeSub"
-
     res = vertexC0tilde(100._dp, 200._dp, 300._dp)
     call vertexC0tildeSub(100._dp, 200._dp, 300._dp, re, im)
 
-    write (*,*) "Result: (real,imag) = ", dble(res).EQ.re, dimag(res).EQ.im
+    eqre = dble(res).EQ.re
+    eqim = dimag(res).EQ.im
+    error = error.OR.eqre.OR.eqim
+
+    write (*,*) "Testing vertexC0tilde == vertexC0tildeSub: (real,imag) = ", eqre, eqim
+
+    if (error) then
+       write (*,*) "Test result: OK"
+    else
+       write (*,*) "Test result: FAIL"
+    end if
 
   End Program Test
